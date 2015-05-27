12f728640a225b4fe7c86be6ead60408419c978d
gy <511955993@qq.com>
2015-05-27 10:51:38 +0800

    Revert "初始化"
    This reverts commit cb7fc0d22acb2980b677c2eca95280850cb745bc.

--- src/com/test/dao/WaterElectDao.java
+++
@@ -1,9 +0,0 @@
-package com.test.dao;
-
-import java.util.List;
-
-import com.test.bean.WaterElect;
-
-public interface WaterElectDao {
-	public WaterElect getWaterElectsByHousernumber(String housernumber);
-}
